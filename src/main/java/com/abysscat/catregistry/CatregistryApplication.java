4fdc019e02bc59eb3b4c5db2532c9bccda0510bc
abysscat-yj <397967552@qq.com>
2024-04-14 01:49:44 +0800

    init project

---
+++ src/main/java/com/abysscat/catregistry/CatregistryApplication.java
@@ -0,0 +1,19 @@
+package com.abysscat.catregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description
+ *
+ * @Author: abysscat-yj
+ * @Create: 2024/4/14 1:48
+ */
+@SpringBootApplication
+public class CatregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatregistryApplication.class, args);
+	}
+
+}
